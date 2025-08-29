103a9428a82c3bb1ff4ccc226c66d20d1d35bb2a
Shan-Min Chao <shanminchao@google.com>
2025-08-29 10:50:46 -0700

    Fix python script parsing issues with latest mesa drop
    Haven't grabbed latest mesa for 2 years!

--- dive_core/common.py
+++ dive_core/common.py
@@ -78,17 +78,8 @@ def isBuiltInType(type):
 
 # ---------------------------------------------------------------------------------------
 def gatherAllEnums(registers_et_root):
-  enums = registers_et_root.findall('{http://nouveau.freedesktop.org/}enum')
-
-  # Find all enums within domain blocks
-  # Those are reserved for use within the domain, but we need to keep track of all
-  # enums in our list, irrespective of scope
-  domains = registers_et_root.findall('{http://nouveau.freedesktop.org/}domain')
-  for domain in domains:
-    domain_enums = domain.findall('{http://nouveau.freedesktop.org/}enum')
-    if domain_enums:
-      for enum in domain_enums:
-        enums.append(enum)
+  # Find all enums anywhere in the tree
+  enums = registers_et_root.findall('.//{http://nouveau.freedesktop.org/}enum')
   return enums
 
 # ---------------------------------------------------------------------------------------
